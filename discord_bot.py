8315e914a20e4a0cfa3b6f51699b688e42570cc6
Opacidablox <thomasdcroz38@gmail.com>
2025-02-19 19:22:58 -0500

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -91,6 +91,117 @@ async def error_type(ctx,error):
     if isinstance(error,commands.errors.MissingRequiredArgument):
         await ctx.send("Tienes que darme un pokemon")
 
+@bot.command()
+async def contaminacion(ctx):
+    await ctx.send(f"""
+    Hola, soy un bot {bot.user}!
+    """)
+    #comentarios
+ 
+    await ctx.send("Quieres conocer de que trata la contaminación?")
+    # Esperar la respuesta del usuario
+    def check(message):
+        return message.author == ctx.author and message.channel == ctx.channel and message.content in ['si', 'no']
+    response = await bot.wait_for('message', check=check)
+    if response:
+        if response.content:
+            await ctx.send(""" 
+            La contaminacion afecta a los grandes ecosistemas naturales que tenemos, como las selvas y los bosques...
+            También hace que la temperatura en el planeta aumente, generando el calentamiendo global.
+            """)
+ 
+        else:
+            await ctx.send("Está bien, si alguna vez necesitas saber sobre otro tema, estaremos en contacto.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+ 
+    await ctx.send("Quieres mas ejemplos sobre contaminación'si' o 'no'.")
+    def check1(message):
+        return message.author == ctx.author and message.channel == ctx.channel and message.content in ['si', 'no']
+    response1 = await bot.wait_for('message', check=check1)
+    if response1:
+        if response1.content == "si":
+            await ctx.send("""
+            El calentamiento global hace que los polos se descongelen, por lo tanto el nivel de los mares aumenta.
+            Provocando así la muerte de la fauna y la flora que habita en esas zonas.
+            """) 
+        else:
+            await ctx.send("Está bien, si alguna vez necesitas hablar sobre otro tema, estaremos en contacto.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+    await ctx.send("Te gustaria que te envie una foto sobre un ejemplo de contaminación?")
+    def check2(message):
+        return message.author == ctx.author and message.channel == ctx.channel and message.content in ['si', 'no']
+    response2 = await bot.wait_for('message', check=check2)
+    if response2:
+        if response1.content == "si":
+            image_path = 'images/contaminacion.jpeg'
+ 
+        # Verificar si el archivo existe antes de enviarlo
+        if os.path.exists(image_path):
+            with open(image_path, 'rb') as f:
+                picture = discord.File(f)
+                await ctx.send("Aquí tienes un ejemplo de contaminación:", file=picture)
+        else:
+            await ctx.send("Lo siento, no pude encontrar la imagen. Verifica que la ruta sea correcta.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+
+@bot.command()
+async def fe2guide(ctx):
+    await ctx.send(f"""
+    Hola, soy un bot {bot.user}!
+    """)
+    #comentarios
+ 
+    await ctx.send("Quieres conocer quien es guide?")
+    # Esperar la respuesta del usuario
+    def check(message):
+        return message.author == ctx.author and message.channel == ctx.channel and message.content in ['si', 'no']
+    response = await bot.wait_for('message', check=check)
+    if response:
+        if response.content:
+            await ctx.send(""" 
+            Guide es un personaje del fe series (Flood Escape) el cual te guiaba y te decia informacion sobre el juego
+            """)
+ 
+        else:
+            await ctx.send("Está bien, si alguna vez necesitas saber sobre otro tema, estaremos en contacto.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+ 
+    await ctx.send("Quieres saber más de guide? 'si' o 'no'.")
+    def check1(message):
+        return message.author == ctx.author and message.channel == ctx.channel and message.content in ['si', 'no']
+    response1 = await bot.wait_for('message', check=check1)
+    if response1:
+        if response1.content == "si":
+            await ctx.send("""
+            El Guide ahora esta en la secuela del juego: fe2 (Flood Escape 2) pero en vez de ser un guía, ahora se volvio
+            uno de los principales personajes de la serie y ahora es uno de las personas que tiene que escapar de la inundación
+            """) 
+        else:
+            await ctx.send("Está bien, si alguna vez necesitas hablar sobre otro tema, estaremos en contacto.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+    await ctx.send("Te gustaria que te envie una foto de quien es él?")
+    def check2(message):
+        return message.author == ctx.author and message.channel == ctx.channel and message.content in ['si', 'no']
+    response2 = await bot.wait_for('message', check=check2)
+    if response2:
+        if response1.content == "si":
+            image_path = 'images/FE2WARRIORS_WIKI_GUIDE.webp'
+ 
+        # Verificar si el archivo existe antes de enviarlo
+        if os.path.exists(image_path):
+            with open(image_path, 'rb') as f:
+                picture = discord.File(f)
+                await ctx.send("Aquí tienes a Guide:", file=picture)
+        else:
+            await ctx.send("Lo siento, no pude encontrar la imagen. Verifica que la ruta sea correcta.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+
 @bot.group()
 async def cool(ctx):
     """Says if a user is cool.
